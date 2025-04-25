8c93fbaf821914ef8f51b62b0b4d1e3e890ce8a4
renatomarf76 <renatomarf76@egmail.com>
2025-04-25 16:02:26 -0300

    02 - Customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button>
-        Hellow world
-      </Button>
+    <div className="p-4">
+      <div className="felx flex-col">
+        <div>
+          <Button variant="elevated">
+            I am a button
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="I am an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea value="I am a textarea" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
