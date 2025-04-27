55295911c4f14edd02baad49352b1a6d5a133d71
renatomarf76 <renatomarf76@egmail.com>
2025-04-27 03:54:37 -0300

    08 - Authentication

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         <TRPCReactProvider>
           {children}
+          <Toaster />
         </TRPCReactProvider>
       </body>
     </html>
